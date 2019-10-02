94062f05af78d916545fbe30ac7993f72d7785bc
Eve Stockstill <evestockstillpdx@gmail.com>
2019-10-02 12:05:42 -0700

    working

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import compareNumbers from '../compareNumbers.js';
+import compareNumbers from './compareNum.js';
 
 const inputNum = document.getElementById('input-number');
 const submitBtn = document.getElementById('btn');
@@ -8,56 +8,40 @@ const youWin = document.getElementById('you-win');
 const tryAgain = document.getElementById('try-again');
 const gameOver = document.getElementById('game-over');
 let triesLeft = 4;
-// let userGuess = ('');
 let correctNumber = 9;
-
 submitBtn.addEventListener('click', () => {
     triesLeft--;
     triesLeftDisplay.textContent = triesLeft;
     const userGuess = parseInt(inputNum.value);
     const result = compareNumbers(userGuess, correctNumber);
 
-    if (result === correctNumber) {
-        tryAgain.classList.add('hidden');
+    if (result === 0) {
+        
         youWin.classList.remove('hidden');
+        tryAgain.classList.add('hidden');
         output.textContent = `Your number ${userGuess} was correct`;
-        userGuess.disabled = true;
+        triesLeftDisplay.textContent = `You got it with ${triesLeft} tries left! Nice!`;
+        submitBtn.disabled = true;
+        triesLeftDisplay.disabled = true;
     } 
-    else if (result > correctNumber && triesLeft < 0) {
+    else if (result === -1 && triesLeft > 0) {
+        
         tryAgain.classList.remove('hidden');
         output.textContent = `Your number ${userGuess} was too low`;
+        triesLeftDisplay.textContent = `you have ${triesLeft} tries left`;
     } 
-    else if (result < correctNumber && triesLeft < 0) {
+    else if (result === 1 && triesLeft > 0) {
+        
         tryAgain.classList.remove('hidden');
         output.textContent = `Your number ${userGuess} was too high`;
-
+        triesLeftDisplay.textContent = `you have ${triesLeft} tries left`;
     } 
-    else (result !== correctNumber && triesLeft === 0); 
-    tryAgain.classList.add('hidden');
-    gameOver.classList.remove('hidden');
-    output.textContent = `You have no more tries left`;
-    userGuess.disabled = true;
+    else {
+    
+        tryAgain.classList.add('hidden');
+        gameOver.classList.remove('hidden');
+        output.textContent = `You have no more tries left`;
+        submitBtn.disabled = true;
+    } 
 });
 
-
-
-
-
-
-
-
-
-
-
-
-// document.getElementById('btn').onclick = () => {
-//     if (secretNumber === guess) {
-//         document.getElementById('output').innerHTML = `You got it! ${guess} is the correct guess!`;
-//     } else if (secretNumber > guess) {
-//         document.getElementById('output').innerHTML = `Try again ${guess} is too low.`;
-//         triesLeft--;
-//     } else if (secretNumber < guess) {
-//         document.getElementById('output').innerHTML = `Oh no ${guess} is too high`;
-//         triesLeft--;
-//     }
-// };
